938f673178bfbbbd875075fcc20a726d92b11785
ubselcuk <ubselcuk@gmail.com>
2024-10-10 12:45:38 +0300

    New web project - ubselcuk - added.

---
+++ ubselcuk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ubselcuk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

c20341c655a0a3e1725bc9ff90ebcb24a08f0aed
ubselcuk <ubselcuk@gmail.com>
2024-10-10 17:38:37 +0300

    Website template added.

---
+++ ubselcuk/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ubselcuk.Controllers
+{
+    public class RootController : Controller
+    {
+        private readonly ILogger<RootController> _logger;
+
+        public RootController(ILogger<RootController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index Called");
+            return View();
+        }
+    }
+}
